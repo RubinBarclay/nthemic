9969c6d6c185a24a2036c012782ce1926c29eb22
Rubin Barclay <rubinbarclay@protonmail.com>
2021-06-15 08:59:20 +0200

    restructured project

--- client/src/containers/Search.jsx
+++ client/src/containers/Search.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SearchField from '../components/SearchField';
 import SearchResults from '../components/SearchResults';
 
 const Search = () => {
+  const [search, setSearch] = useState('');
+  
+  const onSearchHandler = async (string) => {
+    const trackRes = await fetch('https://api.soundcloud.com/tracks?q=' + string);
+    const trackData = await trackRes.json();
+
+    console.log(trackData);
+  }
+
   return (
     <div className="overflow-y-scroll scrollbar-hide col-span-8 grid-cols-12">
-      <SearchField />
+      <SearchField handler={onSearchHandler} />
       <SearchResults type="Songs" />
       <SearchResults type="Artists" />
       <SearchResults type="Playlists" />
